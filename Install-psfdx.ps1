92e5d9e489e21c5f6b5a46d57bfcde8da7686a47
Tony Ward (Admin) <tward.admin@office.zen.co.uk>
2019-06-04 15:03:05 +0100

    bug fix to installation script

--- Install-psfdx.ps1
+++ Install-psfdx.ps1
@@ -2,10 +2,10 @@ $modulespath = ($env:psmodulepath -split ";")[0]
 $pssfdxpath = "$modulespath\psfdx"
 
 Write-Host "Creating module directory"
-New-Item -Type Container -Force -path $pswatchpath | out-null
+New-Item -Type Container -Force -path $pssfdxpath | out-null
 
 Write-Host "Downloading and installing"
-(new-object net.webclient).DownloadString("https://github.com/ZenInternet/psfdx/blob/master/psfdx.psm1") | Out-File "$pssfdxpath\psfdx.psm1" 
+(new-object net.webclient).DownloadString("https://raw.githubusercontent.com/ZenInternet/psfdx/master/psfdx.psm1") | Out-File "$pssfdxpath\psfdx.psm1" 
 
 Write-Host "Installed!"
 Write-Host 'Use "Import-Module psfdx" and then "Get-Command -Module psfdx"'
